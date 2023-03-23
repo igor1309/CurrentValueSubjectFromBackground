f46264527d1aac7b50f054fcbb52e2e6dc19b50e
Igor Malyarov <igor1309@gmail.com>
2023-03-23 14:07:47 +0300

    Initial Commit

---
+++ CurrentValueSubjectFromBackground/CurrentValueSubjectFromBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentValueSubjectFromBackgroundApp.swift
+//  CurrentValueSubjectFromBackground
+//
+//  Created by Igor Malyarov on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrentValueSubjectFromBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
